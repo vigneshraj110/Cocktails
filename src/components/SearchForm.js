e6a79269ebd57bb7846c9ecec2662137f9460fd6
Gokul-Mylsami <gokulmylsami@gmail.com>
2022-06-09 11:00:17 +0530

    files addded

---
+++ src/components/SearchForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useGlobalContext } from "../context";
+
+const SearchForm = () => {
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef("");
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
